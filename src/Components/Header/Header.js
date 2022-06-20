5428bfc4a6bae9560894fb4dfce7ddffe680bc57
gyanAsh <gyanash36@protonmail.com>
2022-06-20 12:39:24 +0530

    Made few minor changes

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './Header.module.css';
+
+const Header = () => {
+  return (
+      <div className={styles.headerContianer}>
+          <img src="" alt="Main page logo"/>
+          <ol>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+          </ol>
+    </div>
+  )
+}
+
+export default Header;

24a1e83ac20b14a6e142562ca130f71a7f06ce5c
gyanAsh <gyanash36@protonmail.com>
2022-06-20 15:26:40 +0530

    Basic nav setup and styling completed

---
+++ src/Components/Contact/Nav/NavContact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavContact = () => {
+  return (
+      <div>
+          
+    </div>
+  )
+}
+
+export default NavContact
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,19 +1,33 @@
 import React from 'react';
 import styles from './Header.module.css';
+import logo from '../../Assets/myLogo.svg';
 
 const Header = () => {
+  const navItem = [
+    { id: 1, title: 'About' },
+    { id: 2, title: 'Experince' },
+    { id: 3, title: 'Work' },
+    { id: 4, title: 'Contact' },
+  ];
   return (
       <div className={styles.headerContianer}>
-          <img src="" alt="Main page logo"/>
-          <ol>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-          </ol>
+      <img src={logo} alt="Main page logo" style={{height:60}} />
+      <ul style={{ listStyleType: 'none' }} className={styles.navContainer}>
+        {navItem.map(({id,title}) => (
+          <NavItem key={id} index={id} text={title} />
+        ))}
+          </ul>
     </div>
   )
 }
 
 export default Header;
+
+const NavItem = ({index,text}) => {
+  return (
+        <li>
+          <span>{index}.</span>
+          <p>{text}</p>
+        </li>
+  )
+}

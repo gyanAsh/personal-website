e9d5dfdd1903facbf67fea06cf085dafffb962be
gyanAsh <gyanash36@protonmail.com>
2022-07-06 21:24:33 +0530

    Text changed in About Page

--- src/Components/Pages/About/About.js
+++ src/Components/Pages/About/About.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const About = ({createPanelsRefs}) => {
   return (
-    <section ref={(e)=>createPanelsRefs(e,1)}><h1>About</h1></section>
+    <section ref={(e)=>createPanelsRefs(e,1)}><h1>About Me</h1></section>
   )
 }
 
